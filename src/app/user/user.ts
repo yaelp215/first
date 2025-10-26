9553abb4f5c16e18effb82e5fdc6e7ab6d28fbf7
פרלמן יעל <1589401@main.local>
2025-10-26 16:01:44 +0200

    user

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {USERS} from '../fake_users'
 
 @Component({
   selector: 'app-user',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
-
+ selectedUser=USERS[0];
+ get imagePath(){
+  return 'assets/users' +this.selectedUser.avatar
+ }
+ changeUser(){
+   const randomlndex=Math.floor(Math.random()*USERS.length);
+   this.selectedUser=USERS[randomlndex];
+ }
 }
