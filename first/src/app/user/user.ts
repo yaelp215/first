0b1c436e13f4e7e9107e0e3be989455de980543c
פרלמן יעל <1589401@main.local>
2025-11-02 16:06:14 +0200

    outputmew

--- first/src/app/user/user.ts
+++ first/src/app/user/user.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import {USERS} from '../fake_users'
 import {  Input,Output } from '@angular/core';
-import { EventEmitter } from '../../../node_modules/@angular/core/discovery.d';
+
 
 @Component({
   selector: 'app-user',
@@ -13,7 +13,7 @@ export class User {
   @Input() name!: string;
   @Input() avatar!: string;
   @Input() id!: string;
-  @Output() userselected=new EventEmitter<string>();
+  @Output() userselected = new EventEmitter<string>();
  
   onselect(){
     this.userselected.emit(this.id)
