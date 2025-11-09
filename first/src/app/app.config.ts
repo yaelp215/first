99c33d39b6ec75a48cf53950716f3ff33a601438
פרלמן יעל <1589401@main.local>
2025-11-09 13:27:50 +0200

    bdika

--- first/src/app/app.config.ts
+++ first/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+
 
 import { routes } from './app.routes';
 
