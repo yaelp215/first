2b9bee65db4fedf5196d9151c98fd8027ce52cae
Yael Perlman <y0583287969@gmail.com>
2025-11-17 16:01:24 +0200

    lesson6

--- first/src/app/tasks/new-task/new-task.ts
+++ first/src/app/tasks/new-task/new-task.ts
@@ -1,12 +1,15 @@
 import { Component  } from '@angular/core';
 import { FormsModule ,NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 
 @Component({
   selector: 'app-new-task',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule,CommonModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
+
 })
 export class NewTask {
   title:string='';
@@ -14,8 +17,8 @@ export class NewTask {
   dueDate:string='';
   onsubmit(from:NgForm){
     console.log(from);
-    console.log(from.title);
+    console.log(from.value.title);
+     console.log(from.value.summary);
+      console.log(from.value.dueDate);
   }
-
-
 }
